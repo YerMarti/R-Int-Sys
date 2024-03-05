5f17cfbf2e232d978d19fd4456955ac3e1439606
Yeray <yeray.martinez@opendeusto.es>
2024-03-05 08:37:53 +0100

    Nose

--- problem/0-problem-template.R
+++ problem/0-problem-template.R
@@ -149,7 +149,7 @@ get.evaluation <- function(state, problem) {
 
 ### LOAD MAP FROM CSV FUNCTION
 load.from.csv <- function(file) {
-  data <- read.csv(file ,header=FALSE) # Read a CSV file
+  data <- read.csv(file, header=FALSE) # Read a CSV file
   
   splitted_txt4 <- str_split(data$V1[[4]], ":")
   values4 <- str_split(splitted_txt4[[1]][2], ";")
@@ -167,4 +167,55 @@ load.from.csv <- function(file) {
   data$cost <- costs
   
   return(data)
+}
+
+load.from.csv.v2 <- function(file){
+  # Read the content of the file
+  file_content <- readLines("../data/multimodal-planner/map4.txt")
+  
+  # Extracting rows, cols, initial_pos, and final_pos
+  map_dimensions <- strsplit(file_content[1], ",")[[1]]
+  rows <- as.integer(map_dimensions[1])
+  cols <- as.integer(map_dimensions[2])
+  
+  initial_pos <- as.integer(strsplit(file_content[2], ",")[[1]])
+  final_pos <- as.integer(strsplit(file_content[3], ",")[[1]])
+  
+  # Creating an empty data frame
+  df <- data.frame(
+    mode = character(),
+    time_cost = integer(),
+    money_cost = double(),
+    stops_list = I(list())
+  )
+  
+  # Parsing each line and adding it to the data frame
+  for (i in 4:length(file_content)) {
+    line <- file_content[i]
+    parts <- strsplit(line, ";")[[1]]
+    
+    mode <- substr(parts[1], 1, 1)
+    time_cost <- as.integer(substr(parts[1], 3, nchar(parts[1])))
+    money_cost <- as.double(parts[2])
+    
+    # Check if stops_list is present
+    stops_list <- ifelse(length(parts) > 2, strsplit(parts[3], ";")[[1]], character(0))
+    
+    # Convert stops_list to a list of vectors (Aquí hay error)
+    stops_list <- if(length(stops_list) > 0) {
+      lapply(stops_list, function(x) as.integer(strsplit(x, ",")[[1]]))
+    } else {
+      list()
+    }
+    
+    df <- rbind(df, data.frame(mode, time_cost, money_cost, stops_list))
+  }
+  
+  # Return the data
+  return(list(rows = rows,
+              cols = cols,
+              initial_pos = initial_pos,
+              final_pos = final_pos,
+              df = df))
+  print(df)
 }
